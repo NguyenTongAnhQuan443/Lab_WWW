f8e2edef0d9d272d6ad720014db827c7ca5951b3
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2024-10-03 21:51:36 +0700

    week 3 update and week2

---
+++ week3/src/main/java/vn/edu/iuh/fit/week3/backend/business/ProductBeanRemote.java
@@ -0,0 +1,4 @@
+package vn.edu.iuh.fit.week3.backend.business;
+
+public class ProductBeanRemote {
+}
---
+++ week3/src/main/java/vn/edu/iuh/fit/week3/backend/repositories/entity/Product.java
@@ -0,0 +1,7 @@
+package vn.edu.iuh.fit.week3.backend.repositories.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product {
+  }
